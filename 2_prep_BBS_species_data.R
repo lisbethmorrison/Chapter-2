e2baf6a83bbd617a2524f4c3f1cf2591e69e3f8d
l.morrison@pgr.reading.ac.uk <l.morrison@pgr.reading.ac.uk>
2019-08-19 15:52:02 +0100

    New files

---
+++ 2_prep_BBS_species_data.R
@@ -0,0 +1,89 @@
+###########################################################
+## Title: Prep BBS species data for analysis 
+## User: Lisbeth Morrison 
+## email: l.morrison@pgr.reading.ac.uk
+## Date: May 2019
+##########################################################
+
+rm(list=ls()) # clear R
+
+#install.packages("dplyr", dep = TRUE)
+library(dplyr)
+
+## read in data
+BBS_2018 <- read.csv("../Data/BBS_abund_data/BBS_data_2018.csv", header=TRUE)
+BBS_2004_2017 <- read.csv("../Data/BBS_abund_data/BBS_data_2004_2017.csv", header=TRUE)
+spp_list <- read.csv("../Data/Trait data/seed_dispersing_spp_list_JAT.csv", header=TRUE)
+
+BBS_2018$YEAR <- as.factor(BBS_2018$YEAR)
+
+### first calculate TOT for 2004-2017 data
+## this is summed across all distance categories and transect sections for each year and visit
+## first change NAs to zeros (for some reason zeros have changed to NAs)
+BBS_2004_2017[is.na(BBS_2004_2017)] <- 0
+
+str(BBS_2004_2017)
+BBS_2004_2017$ï..CBC_CODE <- as.character(BBS_2004_2017$ï..CBC_CODE)
+BBS_2004_2017$ENGLISH_NAME <- as.character(BBS_2004_2017$ENGLISH_NAME)
+BBS_2004_2017$YEAR <- as.factor(BBS_2004_2017$YEAR)
+BBS_2004_2017$BAND_1 <- as.numeric(BBS_2004_2017$BAND_1)
+BBS_2004_2017$BAND_2 <- as.numeric(BBS_2004_2017$BAND_2)
+BBS_2004_2017$BAND_3 <- as.numeric(BBS_2004_2017$BAND_3)
+BBS_2004_2017$FLYING <- as.numeric(BBS_2004_2017$FLYING)
+
+## calculate sum across all distance categories
+BBS_2004_2017$SUM <- rowSums(BBS_2004_2017[,8:11])
+
+## calculate TOT across all transects
+BBS_2004_2017_tot <- BBS_2004_2017 %>%
+  group_by(ï..CBC_CODE, ENGLISH_NAME, YEAR, OBS_DT, VISIT, GRIDREF) %>% 
+  summarise(TOT = sum(SUM))
+BBS_2004_2017_tot <- as.data.frame(BBS_2004_2017_tot)
+head(BBS_2004_2017_tot)
+## change first column to CBC_CODE
+names(BBS_2004_2017_tot)[1]<-paste("CBC_CODE")
+## remove some columns not needed (tenkm and region_code) from 2018 dataset
+BBS_2018 <- BBS_2018[, -c(2:3)]
+head(BBS_2018)
+
+## merge together
+BBS_final <- rbind(BBS_2004_2017_tot, BBS_2018)
+## take max value across both visits
+BBS_final <- BBS_final %>% group_by(YEAR, GRIDREF, ENGLISH_NAME, CBC_CODE) %>% summarise(TOT = max(TOT))
+BBS_final <- as.data.frame(BBS_final)
+head(BBS_final)
+
+## remove mammals from dataset (they have numbers instead of characters as code)
+BBS_final <- subset(BBS_final, !grepl("[0-9]", CBC_CODE))
+## remove rows with zero TOT counts (this removes sheep and some house martin and rook records)
+BBS_final<-BBS_final[!(BBS_final$TOT==0),]
+
+## only include species with at least 8 years of data (not continuous)
+BBS_final2 <- BBS_final %>% group_by(GRIDREF, ENGLISH_NAME, CBC_CODE) %>% summarise(no_years = n())
+## remove site-species combos with less than 8 years of data
+BBS_final2 <- BBS_final2[BBS_final2$no_years>=8,]
+length(unique(BBS_final2$ENGLISH_NAME)) ## 124 species
+length(unique(BBS_final2$GRIDREF)) ## 200 sites still
+
+## merge species and sites into BBS_final
+BBS_final3 <- merge(BBS_final2, BBS_final, by=c("ENGLISH_NAME","CBC_CODE", "GRIDREF"), all=FALSE)
+length(unique(BBS_final3$ENGLISH_NAME)) ## 124 species
+length(unique(BBS_final3$GRIDREF)) ## 200 sites still
+
+## change some common names to merge with trait species list
+BBS_final3$ENGLISH_NAME[ BBS_final3$ENGLISH_NAME == "Common Crossbill" ] <- "Crossbill"
+BBS_final3$ENGLISH_NAME[ BBS_final3$ENGLISH_NAME == "Feral Pigeon" ] <- "Rock Dove"
+
+## merge seed dispersing species list (so we only have BBS data for seed dispersers)
+spp_list <- spp_list[,-c(2,3,5)]
+spp_list <- spp_list[spp_list$yes_no==1,] ## 53 species which are seed dispersers
+BBS_final4 <- merge(BBS_final3, spp_list, by.x="ENGLISH_NAME", by.y="species")
+length(unique(BBS_final4$ENGLISH_NAME)) ## 43 species which have BBS data
+
+
+## save BBS_final
+write.csv(BBS_final4, file="../Data/BBS_abund_data/BBS_2004_2018.csv", row.names=FALSE) ## 200 sites and 124 species from 2004-2018
+spp_list2 <- BBS_final4[,c(1,2)] 
+spp_list2 <- unique(spp_list2)
+## save species list (43 species)
+write.csv(spp_list2, file="../Data/BBS_abund_data/BBS_seed_species_list.csv", row.names=FALSE) ## 124 species
---
+++ 4_ecosystem_function.R
@@ -0,0 +1,80 @@
+###########################################################
+## Title: Calculate mean and stability of function
+## User: Lisbeth Morrison 
+## email: l.morrison@pgr.reading.ac.uk
+## Date: June 2019
+##########################################################
+
+rm(list=ls()) # clear R
+library(tidyverse)
+
+## add data
+BBS_final <- read.csv("../Data/BBS_abund_data/BBS_2004_2018.csv", header=TRUE)
+effect <- read.csv("../Data/Analysis_data/Seed dispersal/seed_effect_traits.csv", header=TRUE) ## 32 spp
+response <- read.csv("../Data/Analysis_data/Seed dispersal/seed_response_traits.csv", header=TRUE) ## 38 spp
+all_traits <- read.csv("../Data/Analysis_data/Seed dispersal/seed_all_traits.csv", header=TRUE) ## 28 spp
+## read these files in to get species list
+
+# ## remove unecessary columns from each trait data (only need name and code to merge with BBS data)
+# ## only need english name and yes/no
+# effect <- effect[,c(1,2)]
+# response <- response[,c(1,2)]
+# effect_response <- effect_response[,c(1,2)]
+
+## merge each file with BBS_final abundance data
+BBS_final_effect <- merge(BBS_final, effect, by=c("ENGLISH_NAME", "CBC_CODE"), all=FALSE)
+length(unique(BBS_final_effect$ENGLISH_NAME)) ## 32 species
+BBS_final_response <- merge(BBS_final, response, by=c("ENGLISH_NAME", "CBC_CODE"), all=FALSE)
+length(unique(BBS_final_response$ENGLISH_NAME)) ## 38 species
+BBS_final_all <- merge(BBS_final, all_traits, by=c("ENGLISH_NAME", "CBC_CODE"), all=FALSE)
+length(unique(BBS_final_all$ENGLISH_NAME)) ## 28 species
+
+############################################################################################################
+############################################################################################################
+#### MEAN FUNCTION
+
+# BBS_mean_effect <- BBS_final_effect %>% group_by(GRIDREF) %>% summarise(Mean_abund = mean(TOT)) ## 32 species at 199 sites
+# BBS_mean_response <- BBS_final_response %>% group_by(GRIDREF) %>% summarise(Mean_abund = mean(TOT)) ## 38 species at 200 sites
+# BBS_mean_effect_response <- BBS_final_effect_response %>% group_by(GRIDREF) %>% summarise(Mean_abund = mean(TOT)) ## 28 species at 199 sites
+
+## wrong way to calculate mean abundance --> should take total across all years for each species, then mean across all species at each site
+## this is done below anyway to calculate stability
+
+############################################################################################################
+############################################################################################################
+#### STABILITY OF FUNCTION
+## calculate mean and SD for each species and site
+
+### effect species (=32)
+## calc total abundance of each species at each site
+BBS_tot_effect <- BBS_final_effect %>% group_by(GRIDREF,ENGLISH_NAME) %>% summarise(total = sum(TOT))
+## calc mean and SD at each site
+BBS_proxy_effect <- BBS_tot_effect %>% group_by(GRIDREF) %>% summarise(mean = mean(total), sd=sd(total))
+BBS_proxy_effect$CV <- BBS_proxy_effect$sd/BBS_proxy_effect$mean ## calculate CV (=SD/mean)
+BBS_proxy_effect$stability <- 1/BBS_proxy_effect$CV ## calculate stability (1/CV)
+
+## response species (=38)
+BBS_tot_response <- BBS_final_response %>% group_by(GRIDREF,ENGLISH_NAME) %>% summarise(total = sum(TOT))
+## calc mean and SD at each site
+BBS_proxy_response <- BBS_tot_response %>% group_by(GRIDREF) %>% summarise(mean = mean(total), sd=sd(total))
+BBS_proxy_response$CV <- BBS_proxy_response$sd/BBS_proxy_response$mean ## calculate CV (=SD/mean)
+BBS_proxy_response$stability <- 1/BBS_proxy_response$CV ## calculate stability (1/CV)
+
+## all trait species (=28)
+BBS_tot_all <- BBS_final_all %>% group_by(GRIDREF, ENGLISH_NAME) %>% summarise(total = sum(TOT))
+## calc mean and SD at each site
+BBS_proxy_all <- BBS_tot_all %>% group_by(GRIDREF) %>% summarise(mean = mean(total), sd=sd(total))
+BBS_proxy_all$CV <- BBS_proxy_all$sd/BBS_proxy_all$mean ## calculate CV (=SD/mean)
+BBS_proxy_all$stability <- 1/BBS_proxy_all$CV ## calculate stability (1/CV)
+
+## remove SD and total columns from each
+BBS_proxy_effect <- BBS_proxy_effect[,-c(3:4)]
+BBS_proxy_response <- BBS_proxy_response[,-c(3:4)]
+BBS_proxy_all <- BBS_proxy_all[,-c(3:4)]
+
+## save files
+write.csv(BBS_proxy_effect, file="../Data/Analysis_data/Seed dispersal/BBS_proxy_seed_effect.csv", row.names=FALSE) ## 199 sites
+write.csv(BBS_proxy_response, file="../Data/Analysis_data/Seed dispersal/BBS_proxy_seed_response.csv", row.names=FALSE) ## 200 sites
+write.csv(BBS_proxy_all, file="../Data/Analysis_data/Seed dispersal/BBS_proxy_seed_all.csv", row.names=FALSE) ## 199 sites
+
+
