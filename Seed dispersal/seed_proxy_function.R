134d3f73b6935691d81ca3323687eea16ecf82f8
lisbethmorrison <l.hordley@pgr.reading.ac.uk>
2021-02-09 14:37:13 +0000

    Re-done analysis for new complete case and detectability datasets

--- Seed dispersal/seed_proxy_function.R
+++ Seed dispersal/seed_proxy_function.R
@@ -9,25 +9,22 @@ rm(list=ls()) # clear R
 library(tidyverse)
 
 ## add data
-BBS_final <- read.csv("../Data/BBS_abund_data/BBS_2004_2018_seed_interpol_repeat_det2.csv", header=TRUE)
+#BBS_final <- read.csv("../Data/BBS_abund_data/BBS_2004_2018_seed_det_comp.csv", header=TRUE)
+BBS_final <- read.csv("../Data/BBS_abund_data/BBS_2004_2018_seed_det_interpol.csv", header=TRUE)
 
-length(unique(BBS_final$ENGLISH_NAME)) ## 39 species
-length(unique(BBS_final$GRIDREF)) ## 200 sites (180 for det1)
+length(unique(BBS_final$ENGLISH_NAME)) ## 45 species // 48 species
+length(unique(BBS_final$GRIDREF)) ## 130 sites // 200 sites
 length(unique(BBS_final$YEAR)) ## 15 years
 
-## remove crane from the analysis - no data on SSI
-BBS_final <- BBS_final[!BBS_final$ENGLISH_NAME=="Crane",]
-BBS_final <- droplevels(BBS_final)
-
 ## create species richness data frame for each site
-## number of rows (i.e. number of species) for each site, year and iteration
-spp_rich_seed <- BBS_final %>% group_by(GRIDREF,YEAR,i) %>% summarise(n=n())
+## number of rows (i.e. number of species) for each site and year
+spp_rich_seed <- BBS_final %>% group_by(GRIDREF,YEAR) %>% summarise(n=n())
 ## take average spp richness over iterations
 spp_rich_seed <- spp_rich_seed %>% group_by(GRIDREF,YEAR) %>% summarise(average=mean(n))
 ## take average spp richness over time
 spp_rich_seed <- spp_rich_seed %>% group_by(GRIDREF) %>% summarise(average_spp_richness=mean(average))
 ## save file
-write.csv(spp_rich_seed, file="../Data/Analysis_data/Seed dispersal/mean_spp_richness.csv", row.names=FALSE) ## 200 sites
+write.csv(spp_rich_seed, file="../Data/Analysis_data/Seed dispersal/mean_spp_richness_det_interpol.csv", row.names=FALSE) ## 200 sites
 
 ############################################################################################################
 ############################################################################################################
@@ -35,18 +32,19 @@ write.csv(spp_rich_seed, file="../Data/Analysis_data/Seed dispersal/mean_spp_ric
 ## calculate total abundance over time at each site
 ## the calculate mean and SD for each site, use this to calculate stability
 
-## calculate this for all 40 species which are seed-eaters for each reptition of abundance interpolation (100 repeats)
-## tot_abund is the total community abundance at each site for each iteration and year
-BBS_tot <- BBS_final %>% group_by(GRIDREF,YEAR,i) %>% summarise(tot_abund = sum(TOT_det))
+## calculate this for all 45 // 48 species which are seed-eaters 
+## tot_abund is the total community abundance at each site for each year
+BBS_tot <- BBS_final %>% group_by(GRIDREF,YEAR) %>% summarise(tot_abund = sum(TOT_det))
 ## calculate the mean abund and SD over time for each site
-BBS_tot2 <- BBS_tot %>% group_by(GRIDREF, i) %>% summarise(mean_abund=mean(tot_abund), sd=sd(tot_abund))
+BBS_tot2 <- BBS_tot %>% group_by(GRIDREF) %>% summarise(mean_abund=mean(tot_abund), sd=sd(tot_abund))
 ## calculate CV 
 BBS_tot2$CV <- BBS_tot2$sd/BBS_tot2$mean_abund
 ## caculate stability
 BBS_tot2$stability <- 1/BBS_tot2$CV
-## take mean stability and mean mean_abund for each site across repetitions
-BBS_proxy <- BBS_tot2 %>% group_by(GRIDREF) %>% summarise(stability=mean(stability), mean_abund=mean(mean_abund))
+## keep mean stability and mean mean_abund for each site
+BBS_proxy <- BBS_tot2[,c(1,2,5)] 
 
 ## save file
-write.csv(BBS_proxy, file="../Data/Analysis_data/Seed dispersal/BBS_proxy_seed_det2.csv", row.names=FALSE) ## 200 sites
-## 180 sites, 39 species for det1
+write.csv(BBS_proxy, file="../Data/Analysis_data/Seed dispersal/BBS_proxy_seed_det_interpol.csv", row.names=FALSE) ## 200 sites
+## 130 sites, 45 species for complete case detectability
+## 200 sites, 48 species for interpolated detectability
